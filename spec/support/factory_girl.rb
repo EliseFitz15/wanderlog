6c7dc3bfe7d80ca8f7c00a00f78871d2b97478d9
Elise Fitzgerald <elise.fitzgerald15@gmail.com>
2015-07-12 13:17:34 -0400

    user updates profile, add name to user

--- spec/support/factory_girl.rb
+++ spec/support/factory_girl.rb
@@ -2,6 +2,7 @@ require 'factory_girl'
 
 FactoryGirl.define do
   factory :user do
+    name 'Charlotte'
     sequence(:email) {|n| "user#{n}@example.com" }
     password 'password'
     password_confirmation 'password'
