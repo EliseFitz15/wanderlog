b89c06602a1675ff92ba31ddd31c484e9199d441
Elise Fitzgerald <elise.fitzgerald15@gmail.com>
2015-07-13 09:34:23 -0400

    update hound and view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,10 +8,24 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(
-      :name, :email, :password, :password_confirmation, :remember_me) }
+      :name,
+      :email,
+      :password,
+      :password_confirmation,
+      :remember_me
+    ) }
     devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(
-      :login, :name, :email, :password, :remember_me) }
+      :login,
+      :name,
+      :email,
+      :password,
+      :remember_me
+    ) }
     devise_parameter_sanitizer.for(:account_update) { |u| u.permit(
-      :name, :email, :password, :password_confirmation, :current_password) }
+      :name,
+      :email,
+      :password,
+      :password_confirmation, :current_password
+    ) }
   end
 end
