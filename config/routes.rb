fa0aa1e65b756a6389865e597b5cfe29993d3e3a
Elise Fitzgerald <elise.fitzgerald15@gmail.com>
2015-07-13 14:13:59 -0400

    style and hound fixes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'homes#index'
-  get '/about'=> 'homes#about'
-  get '/wander'=> 'homes#wander'
+  get '/about' => 'homes#about'
+  get '/wander' => 'homes#wander'
 
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
