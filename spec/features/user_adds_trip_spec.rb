8949e2a045aed9e02f01fe2fe6adbc3ada592356
Elise Fitzgerald <elise.fitzgerald15@gmail.com>
2015-07-14 13:48:29 -0400

    add user test for unsuccessful post

--- spec/features/user_adds_trip_spec.rb
+++ spec/features/user_adds_trip_spec.rb
@@ -28,9 +28,30 @@ so that I can log my travel adventures in one place" do
     fill_in "Startdate", with: trip.startdate
 
     click_button "Create Trip"
-    
+
     expect(page).to have_content("Trip added to your dashboard!")
     expect(page).to have_content("Charlotte's Trips")
     expect(page).to have_content(trip.title)
+  end
+  scenario "user tries to create trip unsuccessfully" do
+    user = FactoryGirl.create(:user)
+    trip = FactoryGirl.create(:trip, user: user)
+
+    visit new_user_session_path
+
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+
+    click_button 'Log in'
+
+    visit trips_path
+
+    click_link "Add New Trip"
+    expect(page).to have_content("Where will you wander?")
+
+    click_button "Create Trip"
+
+    expect(page).to have_content("can't be blank")
+    expect(page).to_not have_content("Charlotte's Trips")
   end
 end
