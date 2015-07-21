634443739b03d348a5e82a4607f2a85cc03d2192
Elise Fitzgerald <elise.fitzgerald15@gmail.com>
2015-07-21 18:32:39 -0400

    adds countdown to trip show

---
+++ db/migrate/20150721211356_change_to_datetime_on_trips.rb
@@ -0,0 +1,10 @@
+class ChangeToDatetimeOnTrips < ActiveRecord::Migration
+  def up
+    remove_column :trips, :startdate, :date, null: false
+    add_column :trips, :startdate, :datetime, null: false
+  end
+  def down
+    remove_column :trips, :startdate, :datetime, null: false
+    add_column :trips, :startdate, :date, null: false
+  end
+end
